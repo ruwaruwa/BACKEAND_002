5a6fdb7b5af5187c6b7b3f7d7f0eec0020b9e5bf
ruwaruwa <almoz287@gmail.com>
2023-10-01 10:14:08 +0300

    new update

--- Ddconfig/DBconection.js
+++ Ddconfig/DBconection.js
@@ -4,9 +4,9 @@ const DB_CON=async()=>{
         //mongodb://127.0.0.1:27017/Houses
         //this is the connection to the database 
         mongoose.connect("mongodb+srv://amiira123:12345@cluster0.xdvvzfc.mongodb.net/work");
-        console.log('DB connected !')
+        console.log('DB connected !!')
     } catch (error) {
-       console.log(error.message) 
+       console.log(error.message);
     }
 }
 module.exports = DB_CON
